1cadc31bc972eabcd45e3b349b9b5881b411b283
guerre99 <dgr761@gmail.com>
2023-10-16 23:26:14 +0200

    fix:index.js

--- js/index.js
+++ js/index.js
@@ -3,83 +3,30 @@ const ctx = canvas.getContext('2d')
 let canvasW = canvas.width = innerWidth
 let canvasH = canvas.height = innerHeight
 
-let x = 400
-let y = 30
+let x = 100
+let y = 100
 let ancho = 50
 let largo = 50
 let dx = 1
-let dy = 1
-let y0 = y
+let dy = 50
+let y0 = 0
 
 
 function move(){
-    if (x >= (canvas.width - largo)){ 
+    if(x > canvasW - ancho || x < 0){
         dx *= -1
-        x = (canvas.width - largo)
     }
-
-    if(x === (canvas.width - largo)){
+    else if (x == canvasW -ancho || x == 0){
         y += dy
-        console.log(y)
-        if (y >= y0 + 100*dy){
-            y0 = y
-            dy= 0
-        }else x -= dx
-    }
-
-    if(x <= 0){
-        dx *= -1
-        x += 0
-        console.log(x)
-    }
-
-    if (x === 0){
-        dy = 2
-        y += dy
-        console.log(x,y, y0 + 50*dy)
-        if (y >= y0 + 50*dy){
-            y0 = y
-            dy= 0
-        }else x += dx
     }
    
     x += dx
 }
 
 
-// function move(){
-    
-
-//     for(let i = 0;i<5;i++){
-//         if(i%2 == 0){
-//             for(j=0;j<2;j++){
-//                 if(j=0 && x <=500){
-//                     x += dx
-//                 }
-//                 else if(j=1 && x == 500){
-//                     y+=dy
-//                 }
-//             }
-//         }
-//         if(i%2 !== 0){
-//             for(j=0;j<2;j++){
-//                 if(j=0 && x >=20){
-//                     x -= dx
-//                 }
-//                 else if(j=1 && x == 20){
-//                     y+=dy
-//                 }
-//             }
-//         }
-//     }
-// }
-
-const fondo = new Background(ctx,canvas.width,canvas.height)
-
 function draw(){
     ctx.fillStyle= 'red'
     ctx.fillRect(x,y,50,50)
-    this.fondo.draw()
 
 }
 
@@ -89,7 +36,6 @@ this.intervalId = setInterval(() => {
     frameCounter ++
 
     ctx.clearRect(0, 0,canvas.width,canvas.height)
-    // console.log('test')
     draw()
     move()
 
